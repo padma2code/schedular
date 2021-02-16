cceba3fe77ece2d2d3179f43636d888082f4cf92
Padma2Code <padmavalli.aluri@gmail.com>
2021-02-15 21:53:20 -0500

    refactored  button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = classNames("button", {
+   const buttonClass = classNames("button", {
       "button--confirm": props.confirm,
       "button--danger": props.danger
    });
