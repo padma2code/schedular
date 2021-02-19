918a4099792dc7fcdc63e04d183407b97bfca801
Padma2Code <padmavalli.aluri@gmail.com>
2021-02-19 00:27:42 -0500

    create and test getInterview selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,15 +1,26 @@
-export default function getAppointmentsForDay(state, day) {
-    const result = [];
-    const dayData = state.days.filter(d => d.name === day)
-    if (!dayData[0]) return result;
-    for (const a of dayData[0].appointments) {
-      result.push(state.appointments[a]);
-    }
-    return result;
-  
-    // if (!state) return []
-    // const dayData = state.days.filter(d => d.name === day)
-    // if (dayData.length === 0) return [];
-    // return dayData[0].appointments.reduce((cum, cur) => cum.concat(state.appointments[cur]), [])
-  };
-  
+function getAppointmentsForDay(state, day) {
+  const result = [];
+  const dayData = state.days.filter(d => d.name === day)
+  if (!dayData[0]) return result;
+  for (const a of dayData[0].appointments) {
+    result.push(state.appointments[a]);
+  }
+  return result;
+
+  // Showed by a mentor but to uncomfortable with reduce to use/understand it
+  // if (!state) return []
+  // const dayData = state.days.filter(d => d.name === day)
+  // if (dayData.length === 0) return [];
+  // return dayData[0].appointments.reduce((cum, cur) => cum.concat(state.appointments[cur]), [])
+};
+
+function getInterview(state, interview) {
+  if (interview) {
+    const interviewer = state.interviewers[interview.interviewer];
+    // const interviewerData = state.interviewers.filter(i => i.id === interview.interviewer)
+    return { ...interview, interviewer };
+  }
+  return null;
+}
+
+export { getAppointmentsForDay, getInterview };
