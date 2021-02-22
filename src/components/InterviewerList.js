2ecece89c13454896dfa3466c65b221fa131618c
Padma2Code <padmavalli.aluri@gmail.com>
2021-02-22 16:25:32 -0500

    install prop types & validate value and onChange props with prop types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,15 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
+  InterviewerList.propTypes = {
+    value: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+  };
+  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
