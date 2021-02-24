1845222b5ff6404947b8078d5306a0fc9c5eddd0
Padma2Code <padmavalli.aluri@gmail.com>
2021-02-23 23:28:52 -0500

    add endpoints for the edit and delete tests

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -89,6 +89,19 @@ const fixtures = {
           statusText: "No Content"
         })
       }
+      if (url === "/api/appointments/2") {
+        return Promise.resolve({
+          status: 204,
+          statusText: "No Content"
+        })
+      }
+    }),
+    delete: jest.fn(url => {
+      if (url === "/api/appointments/2") {
+        return Promise.resolve({
+          status: 200,
+          statusText: "OK"
+        })
+      }
     })
   }
-  
