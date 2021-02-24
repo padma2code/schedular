6a1fcc6410d04220b1efb755a345d97f22c6eebf
Padma2Code <padmavalli.aluri@gmail.com>
2021-02-23 23:01:43 -0500

    add a put request to mock axios.put

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -81,5 +81,14 @@ const fixtures = {
           data: fixtures.interviewers
         })
       }
+    }),
+    put: jest.fn(url => {
+      if (url === "/api/appointments/1") {
+        return Promise.resolve({
+          status: 204,
+          statusText: "No Content"
+        })
+      }
     })
   }
+  
